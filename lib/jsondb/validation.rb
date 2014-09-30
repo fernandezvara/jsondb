39719415caaf11e8d6c25787d8482f7c774181db
Antonio Fernandez <antonio.fernandez@bq.com>
2014-09-30 11:39:08 +0200

    removed fields.rb, not needed anymore. Validations ordered

--- lib/jsondb/validation.rb
+++ lib/jsondb/validation.rb
@@ -1,7 +1,7 @@
 module Validation
 
 	def allowed_types
-		["String", "Fixnum", "Integer", "Float", "Date", "Bool"]
+		["String", "Fixnum", "Integer", "Float", "Time", "Bool"]
 	end
 
 	def allowed?(type)
@@ -13,47 +13,66 @@ module Validation
 	end
 
 	def validate_type(_class, _value)
-		#_class = eval(_class) if _class.class.to_s == "String"
 		case _class
 		when "Bool"
-			case _value
-			when true, false
-				return _value
-			else
-				raise "Validation: Bool: Not a Bool type"
-			end
+			bool?(_class, _value)
 		when "String"
-			if _class == _value.class.to_s
-				return _value
-			else
-				raise "Validation: #{_class}: Value '#{_value}' not allowed for '#{_class}' type"
-			end
+			string?(_class, _value)
 		when "Fixnum", "Integer", "Float"
-			case _class
-			when "Integer"
-				if eval(_value).class.to_s == "Integer"
-					return _value
-				else
-					raise "Validation: '#{_value}' is not Integer"
-				end
-      when "Fixnum"
-        if _value.class.to_s == "Fixnum"
-          return _value
-        else
-          raise "Validation: '#{_value}' is not Integer"
-        end
-			when "Float"
-				case eval(_value).class.to_s
-				when "Float"
-					return _value
-				when "Integer"
-					return _value.to_f
-				end
-			else
-				raise "Validation: '#{_value} is not a Numeric type"
-			end
+			numeric?(_class, _value)
+		when "Time"
+			time?(_class, _value)
 		else
 			raise "Validation: '#{_class}' type not allowed"
 		end
 	end
+
+	private
+	
+	def bool?(_class, _value)
+		case _value
+		when true, false
+			return _value
+		else
+			raise "Validation: Bool: Not a Bool type"
+		end
+	end
+
+	def string?(_class, _value)
+		if _class == _value.class.to_s
+			return _value
+		else
+			raise "Validation: #{_class}: Value '#{_value}' not allowed for '#{_class}' type"
+		end
+	end
+
+	def numeric?(_class, _value)
+		case _class
+	  when "Fixnum", "Integer"
+	    if _value.class.to_s == "Fixnum"
+	      return _value
+	    else
+	      raise "Validation: '#{_value}' is not Integer"
+	    end
+		when "Float"
+			case eval(_value).class.to_s
+			when "Float"
+				return _value
+			when "Fixnum"
+				return _value.to_f
+			else
+				raise "Validation: '#{_value} is not a Float type"	
+			end
+		else
+			raise "Validation: '#{_value} is not a Numeric type"
+		end
+	end
+
+	def time?(_class, _value)
+		if eval(_value).class.to_s != "Time"
+			raise "Validation: '#{_value} is not a Time type" 
+		end
+	end
+	
 end
+
